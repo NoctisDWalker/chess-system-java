ea1286b97906a40868f9dcd2fde878c7a52d9875
Filipe dos Santos <filipeuvft@gmail.com>
2025-08-14 20:39:25 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,17 +6,21 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.awt.image.ImagingOpException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
 
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printMath(chessMatch);
+                UI.printMatch(chessMatch, captured);
+
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -24,12 +28,16 @@ public class Program {
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
+
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
@@ -38,7 +46,5 @@ public class Program {
                 sc.nextLine();
             }
         }
-
-
     }
 }
